b41b6ce35d17f4b0560bdde3a1da3cdfeca0cf5e
Kiran <kiran.tech.one@gmail.com>
2013-07-07 12:05:47 +0000

    web.js reads from index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+   fs.readFileSync('index.html', function (error, data) {
+     if(err) throw err;
+     console.log(data);
+     var rsp = Buffer.toString(data);
+     response.send(rsp);
+   });
+   //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
