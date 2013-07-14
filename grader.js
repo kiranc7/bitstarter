73610fcc5d8db58c0c5a3980bc4b92df5d22f33e
Kiran <kiran.tech.one@gmail.com>
2013-07-14 05:36:23 +0000

    modified grader to accept url

--- grader.js
+++ grader.js
@@ -24,7 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
+var HTMLURL_DEFAULT = "http://obscure-earth-9537.herokuapp.com/";
+var URLFILE_DEFAULT = "url.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -61,14 +64,29 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var buildfn = function(urlfile, checks) {
+    var response2console = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            //console.error("Wrote %s", urlfile);
+            fs.writeFileSync(urlfile, result);
+            var checkJson = checkHtmlFile(urlfile, checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+        }
+    };
+    return response2console;
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        //.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url [html_url]', 'URL to index.html', HTMLURL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var response2console = buildfn(URLFILE_DEFAULT, program.checks);
+    rest.get(program.url).on('complete', response2console);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
